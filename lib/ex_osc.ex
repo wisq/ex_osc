c8107e1c84f55d15ba4e1d5fcb18f1f759196282
Adrian Irving-Beer <wisq@wisq.net>
2023-04-15 22:11:29 -0400

    `mix new ex_osc`

---
+++ lib/ex_osc.ex
@@ -0,0 +1,18 @@
+defmodule ExOsc do
+  @moduledoc """
+  Documentation for `ExOsc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExOsc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_osc_test.exs
@@ -0,0 +1,8 @@
+defmodule ExOscTest do
+  use ExUnit.Case
+  doctest ExOsc
+
+  test "greets the world" do
+    assert ExOsc.hello() == :world
+  end
+end
