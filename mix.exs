d19b61e3564f51d1e1ef7faa09ecf219e77b5fec
Adrian Irving-Beer <wisq@wisq.net>
2023-04-19 21:11:45 -0400

    Prepare for hex publishing.

--- mix.exs
+++ mix.exs
@@ -1,5 +1,7 @@
-defmodule ExOsc.MixProject do
+defmodule ExOSC.MixProject do
   use Mix.Project
+
+  @github_url "https://github.com/wisq/ex_osc"
 
   def project do
     [
@@ -7,7 +9,11 @@ defmodule ExOsc.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      docs: docs(),
+      deps: deps(),
+      source_url: @github_url
     ]
   end
 
@@ -15,6 +21,35 @@ defmodule ExOsc.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description do
+    """
+    ExOSC is a library for sending and receiving messages to/from audio
+    hardware that supports the OpenSoundControl 1.0 protocol.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Adrian Irving-Beer"],
+      licenses: ["MIT"],
+      links: %{GitHub: @github_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: [
+        "README.md"
+      ],
+      groups_for_modules: [
+        "Client API": ~r/^ExOSC\./,
+        "OSC Protocol": ~r/^OSC\./
+      ]
     ]
   end
 
